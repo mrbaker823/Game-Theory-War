d47375e7bd6f1e0e7976bc345a51fbf64d8d40ac
mrbaker823 <mrbaker823@gmail.com>
2012-10-21 16:31:40 -0500

    Initial application

---
+++ Game Theory War/gen/com/baker/gtgame/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.baker.gtgame;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Game Theory War/src/com/baker/gtgame/WarGame.java
@@ -0,0 +1,36 @@
+package com.baker.gtgame;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Menu;
+import com.baker.gtgame.Board;
+
+public class WarGame extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_war_game);
+        
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        
+        System.out.printf("%d x %d",width,height);
+        
+        System.out.println(Board.BOARDSIZE);
+        System.out.println("1");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_war_game, menu);
+        return true;
+    }
+
+    
+}
